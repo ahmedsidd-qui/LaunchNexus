467a81304d4fdb6287d187d0baee5926ae7a1a16
Muhammad Ahmed <ahmedsidd_qui@outlook.com>
2024-02-09 01:18:39 +0500

    day2

--- main.js
+++ main.js
@@ -1,18 +1,65 @@
-const body = document.body;
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-const headerHeight = document.querySelector("header").offsetHeight;
-main.style.top = headerHeight + "px";
-let lastScroll = 0;
-window.addEventListener("scroll", () => {
-  let currentScroll = window.pageYOffset;
-  if (currentScroll < lastScroll) {
-    // scrolled up -- header show
-    header.classList.add("scroll-up");
-    header.classList.remove("scroll-down");
-  } else {
-    header.classList.remove("scroll-up");
-    header.classList.add("scroll-down");
-  }
-  lastScroll = currentScroll;
-});
+// let data =[
+//     {
+//         heading :"Frontend Development",
+//         content :"Craft user interfaces that come to life in the browser."
+//     },
+//     {
+//         heading :"Backend Development",
+//         content :"Build the robust engine that powers web applications."
+//     },
+//     {
+//         heading :"Wordpress",
+//         content :"Master the world's most popular content management system."
+//     },
+//     {
+//         heading :"C++",
+//         content :"Unleash the power of performance with this versatile programming language."
+//     },
+//     {
+//         heading :"Java",
+//         content :"Dive into the enterprise-grade platform for robust applications."
+//     },
+//     {
+//         heading :"Python",
+//         content :"Explore the versatile language for web, data science, and more."
+//     },
+//     {
+//         heading :"Graphic Design",
+//         content :"Create visually stunning assets for websites, apps, and beyond."
+//     },
+//     {
+//         heading :"Figma Design",
+//         content :"Collaborate and design modern interfaces in the industry-standard tool."
+//     },
+//     {
+//         heading :"UI/UX Design",
+//         content :"Shape user experiences that are both beautiful and functional."
+//     },
+//     {
+
+//         heading: "Application Development using Flutter",
+//         content: "Build native mobile apps with Google's cross-platform framework.",
+//     },
+// ];
+// for (let index = 0; index < data.length; index++) {
+//     document.getElementById("cards").innerHTML+=`
+//     <h3><center>${data[index].heading}</center></h3>
+//     <p>${data[index].content}</p>
+//     <button><a target="_blank" href="https://forms.gle/mTf8i1xr8LAMUFcdA">Apply Now! </a></button>
+//     `
+    
+// }
+document.getElementById("row").innerHTML  += `
+<nav>
+<div class="logo">
+<a href="index.html"><img src="./assets/images/Screenshot 2024-02-08 193152.png" alt=""></a>
+</div>
+<div class="nav-items">
+    <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="internship.html">Intership</a></li>
+    </ul>
+</div>
+</nav>
+`;
