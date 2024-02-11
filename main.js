272004fd25389d85df9bc4a5ff07bfb714d619d9
Muhammad Ahmed <ahmedsidd_qui@outlook.com>
2024-02-12 04:29:41 +0500

    day5

--- main.js
+++ main.js
@@ -1,56 +1,5 @@
-// let data =[
-//     {
-//         heading :"Frontend Development",
-//         content :"Craft user interfaces that come to life in the browser."
-//     },
-//     {
-//         heading :"Backend Development",
-//         content :"Build the robust engine that powers web applications."
-//     },
-//     {
-//         heading :"Wordpress",
-//         content :"Master the world's most popular content management system."
-//     },
-//     {
-//         heading :"C++",
-//         content :"Unleash the power of performance with this versatile programming language."
-//     },
-//     {
-//         heading :"Java",
-//         content :"Dive into the enterprise-grade platform for robust applications."
-//     },
-//     {
-//         heading :"Python",
-//         content :"Explore the versatile language for web, data science, and more."
-//     },
-//     {
-//         heading :"Graphic Design",
-//         content :"Create visually stunning assets for websites, apps, and beyond."
-//     },
-//     {
-//         heading :"Figma Design",
-//         content :"Collaborate and design modern interfaces in the industry-standard tool."
-//     },
-//     {
-//         heading :"UI/UX Design",
-//         content :"Shape user experiences that are both beautiful and functional."
-//     },
-//     {
-
-//         heading: "Application Development using Flutter",
-//         content: "Build native mobile apps with Google's cross-platform framework.",
-//     },
-// ];
-// for (let index = 0; index < data.length; index++) {
-//     document.getElementById("cards").innerHTML+=`
-//     <h3><center>${data[index].heading}</center></h3>
-//     <p>${data[index].content}</p>
-//     <button><a target="_blank" href="https://forms.gle/mTf8i1xr8LAMUFcdA">Apply Now! </a></button>
-//     `
-    
-// }
 document.getElementById("row").innerHTML  += `
-<nav>
+<div class="nav">
 <div class="logo">
 <a href="index.html"><img src="./assets/images/Screenshot 2024-02-08 193152.png" alt=""></a>
 </div>
@@ -61,5 +10,29 @@ document.getElementById("row").innerHTML  += `
         <li><a href="internship.html">Intership</a></li>
     </ul>
 </div>
-</nav>
+</div>
 `;
+
+document.getElementById("footer-all").innerHTML+=`<footer>
+<div class="footer-top">
+<div class="footer-one">
+<h2>LaunchNexus</h2>
+<p>Unlock your potential with hands-on experience in our internship programs.</p>
+</div>
+<div class="footer-two">
+<h2>Get every latest updates</h2>
+<form action="">
+    <input type="email" name="email" id="" required>
+    <span><button id="footer-button" type="submit">Subscribe</button></span>
+</form>
+</div>
+<div class="footer-three">
+<h2>Contact Us</h2>
+<a href=""><i style="color: #808080;" class="fa-brands fa-2xl fa-linkedin"></i></a>
+<a href=""><i style="color: #808080;" class="fa-solid fa-2xl fa-envelope"></i></a>
+<a href=""><i style="color: #808080;" class="fa-brands fa-2xl fa-square-whatsapp"></i></a>
+</div>
+</div>
+<div class="fp"><p><center>&copy;2024  | Made by LaunchNexus</center></p></div>
+
+</footer>`;
